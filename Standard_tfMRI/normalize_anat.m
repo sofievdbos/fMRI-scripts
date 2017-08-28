275d9ce88ec18dd463b47e3603bda2701aa34004
sofievdbos <sofie.vandenbossche@ugent.be>
2017-08-28 18:10:45 +0200

    Add files via upload

---
+++ Standard_tfMRI/normalize_anat.m
@@ -0,0 +1,36 @@
+function [] = normalize_anat(anat_targetdir, anat_file, c, c1, c2, c3, c4, c5, voxel_size, interp, sub_name)
+
+%% Running anatomical normalization.
+
+%anat_targetdir: path to NIfTI anatomical data: anat_targetdir(i,1)
+%anat_file: file output from the previous anat step, i.e. segmentation: '^y_c.*\.*'
+%c/c1/c2/c3/c4/c5: file output from the previous anat step, i.e. segmentation: '^c.*\.*', '^c1.*\.*', '^c2.*\.*', '^c3.*\.*', '^c4.*\.*', '^c5.*\.*'
+%voxel_size: original voxel size of anatomical images; in this example: [1 1 1]
+%interp: interpolation; default value as indicated in the SPM batch: 4 - Roma: 7
+%sub_name: name current subject: sbj_fold(i).name
+
+%% REMARK: do not forget to change the voxel_size to the original voxel size of the anatomical images!
+%%
+
+do_normalize_struct = 1;
+
+if do_normalize_struct
+        %% normalise (write)
+        ibatch = 1;
+        clear matlabbatch;
+        
+        matlabbatch{ibatch}.spm.spatial.normalise.write.subj.def(1) = cellstr(spm_select('FPList', anat_targetdir,  anat_file));
+        matlabbatch{ibatch}.spm.spatial.normalise.write.subj.resample = [cellstr(spm_select('FPList', anat_targetdir, c)); cellstr(spm_select('FPList', anat_targetdir, c1)); ...
+                cellstr(spm_select('FPList', anat_targetdir, c2)); cellstr(spm_select('FPList', anat_targetdir, c3)); ...
+                cellstr(spm_select('FPList', anat_targetdir, c4)); cellstr(spm_select('FPList', anat_targetdir, c5))];      
+        matlabbatch{ibatch}.spm.spatial.normalise.write.woptions.bb = [-90 -126 -72
+            90 90 108];
+        
+        matlabbatch{ibatch}.spm.spatial.normalise.write.woptions.vox = voxel_size;
+        matlabbatch{ibatch}.spm.spatial.normalise.write.woptions.interp = interp;
+        disp('Calculating normalization with SPM batch ----');
+        disp(sprintf(sub_name));
+        spm_jobman('run',matlabbatch);
+        disp('Normalization of anatomical done!');
+end
+end
---
+++ Standard_tfMRI/smoothing.m
@@ -0,0 +1,27 @@
+function [] = smoothing(func_targetdir, funct_file, FWHM, sub_name)
+
+%% Running smoothing.
+
+%func_targetdir: path to NIfTI functional data: func_targetdir(i,1)
+%funct_file: file output from the previous func step; i.e. normalize: '^wrof4D.*\.*'
+%FWHM: 2.5 x voxel size; in this example: [8 8 8] = default - Roma: [6 6 6] 
+%sub_name: name current subject: sbj_fold(i).name
+
+%% REMARK: do not forget to change the FWHM (2.5x voxel size)!
+%%
+
+do_smooth = 1;
+
+if do_smooth
+        clear matlabbatch;
+        ibatch=1;      
+        
+        matlabbatch{ibatch}.spm.spatial.smooth.data = cellstr(spm_select('ExtFPList', func_targetdir, funct_file, Inf));
+        matlabbatch{ibatch}.spm.spatial.smooth.fwhm = FWHM;
+        disp('Running smoothing with SPM batch ----');
+        disp(sprintf(sub_name));
+        disp('     ----      ');
+        spm_jobman('run',matlabbatch);
+        disp('Smoothing done!');
+end
+end
